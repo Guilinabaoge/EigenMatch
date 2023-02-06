3d10a8133dcaef81dc10fed6ce5a12533e6b1225
constantinosskitsas <constantinosskitsas@hotmail.com>
2023-02-06 12:46:54 +0100

    d

--- cs.h
+++ cs.h
@@ -1,58 +1,47 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 #include <Eigen/Core>
 #include "Grm.h"
 using namespace std;
-using  namespace Eigen;
- class CSV{
-    public:  
-//VectorXd eigenvalues;
-VertexID ID;
-vector<pair<VertexID,VertexID>> edges;
-VertexID *edgesQ;
-VertexID *edgesV;
-bool change=true;
-bool Ichange=true;
-bool IPchange=true;
-bool deleted=false;
-public:  
-CSV(int eigens,VertexID IDV){
-//uniquelabels=new int(LBLSize);
-//eigenvalues(eigens);
-//eigenvalues.resize(eigens);
-ID=IDV;
-}
-CSV(int eigens,VertexID IDV,ui maxDeg){
+using namespace Eigen;
+class CSV
+{
+public:
+    // VectorXd eigenvalues;
+    VertexID ID;
+    vector<pair<VertexID, VertexID>> edges;
+    VertexID *edgesQ;
+    VertexID *edgesV;
+    bool change = true;
+    bool Ichange = true;
+    bool IPchange = true;
+    bool deleted = false;
 
-//eigenvalues.resize(eigens);
-ID=IDV;
-//edges.reserve(maxDeg);
-deleted=false;
+public:
+    CSV(int eigens, VertexID IDV)
+    {
 
-}
-CSV(int eigens,ui IDV,ui maxDeg,ui MaxQDeg){
-//eigenvalues.resize(eigens);
-ID=IDV;
-//edgesQ =new VertexID[maxDeg*MaxQDeg];
-//edgesV =new VertexID[maxDeg*MaxQDeg];
-//memset(edgesQ, 0, sizeof(ui) * maxDeg*MaxQDeg);
-//memset(edgesV, 0, sizeof(ui) * maxDeg*MaxQDeg);
-//change=true;
-//Ichange=true;
-}
-CSV(ui IDV,ui totalD){
-ID=IDV;
-//edgesQ =new VertexID[totalD];
-//edgesV =new VertexID[totalD];
-//memset(edgesQ, 0, sizeof(ui) * totalD);
-//memset(edgesV, 0, sizeof(ui) * totalD);
+        ID = IDV;
+    }
+    CSV(int eigens, VertexID IDV, ui maxDeg)
+    {
 
-}
-CSV(){
-    ID=0;
-    deleted=true;
-   // eigenvalues.resize(10);
-    //edges.reserve(500);
-}
+        ID = IDV;
+
+        deleted = false;
+    }
+    CSV(int eigens, ui IDV, ui maxDeg, ui MaxQDeg)
+    {
+
+        ID = IDV;
+    }
+    CSV(ui IDV, ui totalD)
+    {
+        ID = IDV;
+    }
+    CSV()
+    {
+        ID = 0;
+        deleted = true;
+    }
 };
-
